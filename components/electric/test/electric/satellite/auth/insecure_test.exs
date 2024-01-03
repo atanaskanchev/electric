024b62a67e17d2f3212afb7c6d11963ae16f316e
Oleksii Sholik <oleksii@sholik.dev>
2024-01-03 14:35:43 +0200

    Add expires_at key to the Auth struct
    Auth state's expiration can belong together with the user_id. It's not necessary
    to return it as a separate tuple element.

--- components/electric/test/electric/satellite/auth/insecure_test.exs
+++ components/electric/test/electric/satellite/auth/insecure_test.exs
@@ -28,26 +28,28 @@ defmodule Electric.Satellite.Auth.InsecureTest do
 
       token = unsigned_token(claims)
 
-      assert {:ok, %Auth{user_id: "12345"}, claims["exp"]} ==
+      assert {:ok, %Auth{user_id: "12345", expires_at: claims["exp"]}} ==
                validate_token(token, config(namespace: @namespace))
 
       ###
 
       claims = %{"user_id" => "0", "exp" => DateTime.to_unix(~U[2123-05-01 00:00:00Z])}
       token = unsigned_token(claims)
-      assert {:ok, %Auth{user_id: "0"}, claims["exp"]} == validate_token(token, config([]))
+
+      assert {:ok, %Auth{user_id: "0", expires_at: claims["exp"]}} ==
+               validate_token(token, config([]))
     end
 
     test "successfully extracts the namespaced user_id claim" do
       claims = %{"custom_namespace" => %{"user_id" => "000"}}
       token = unsigned_token(claims)
 
-      assert {:ok, %Auth{user_id: "000"}, nil} ==
+      assert {:ok, %Auth{user_id: "000"}} ==
                validate_token(token, config(namespace: "custom_namespace"))
 
       claims = %{"user_id" => "111"}
       token = unsigned_token(claims)
-      assert {:ok, %Auth{user_id: "111"}, nil} == validate_token(token, config(namespace: ""))
+      assert {:ok, %Auth{user_id: "111"}} == validate_token(token, config(namespace: ""))
     end
 
     test "validates the iat claim" do
@@ -114,7 +116,7 @@ defmodule Electric.Satellite.Auth.InsecureTest do
 
       token = signed_token(claims)
 
-      assert {:ok, %Auth{user_id: "12345"}, claims["exp"]} ==
+      assert {:ok, %Auth{user_id: "12345", expires_at: claims["exp"]}} ==
                validate_token(token, config(namespace: @namespace))
     end
 
@@ -122,12 +124,12 @@ defmodule Electric.Satellite.Auth.InsecureTest do
       claims = %{"custom_namespace" => %{"user_id" => "000"}}
       token = signed_token(claims)
 
-      assert {:ok, %Auth{user_id: "000"}, nil} ==
+      assert {:ok, %Auth{user_id: "000"}} ==
                validate_token(token, config(namespace: "custom_namespace"))
 
       claims = %{"user_id" => "111"}
       token = signed_token(claims)
-      assert {:ok, %Auth{user_id: "111"}, nil} == validate_token(token, config(namespace: ""))
+      assert {:ok, %Auth{user_id: "111"}} == validate_token(token, config(namespace: ""))
     end
 
     test "validates the iat claim" do
