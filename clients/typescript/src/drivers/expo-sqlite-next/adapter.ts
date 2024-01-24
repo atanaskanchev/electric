94530ca4de99ce59791f7df27a6b08d983f16dd2
msfstef <stefanos@electric-sql.com>
2024-01-24 15:50:15 +0200

    Use base interface to implement expo adapter

--- clients/typescript/src/drivers/expo-sqlite-next/adapter.ts
+++ clients/typescript/src/drivers/expo-sqlite-next/adapter.ts
@@ -9,6 +9,17 @@ export class DatabaseAdapter extends SerialDatabaseAdapter {
   constructor(db: Database) {
     super()
     this.db = db
+    this.txMutex = new Mutex()
+  }
+
+  async run(statement: Statement): Promise<RunResult> {
+    await this.txMutex.waitForUnlock()
+    return this._run(statement)
+  }
+
+  async query(statement: Statement): Promise<Row[]> {
+    await this.txMutex.waitForUnlock()
+    return this._query(statement)
   }
 
   async _run(statement: Statement): Promise<RunResult> {
@@ -25,5 +36,80 @@ export class DatabaseAdapter extends SerialDatabaseAdapter {
   async _query(statement: Statement): Promise<Row[]> {
     const { sql: source, args: params = [] } = statement
     return await this.db.getAllAsync(source, params as Omit<SqlValue, 'bigint'>)
+  }
+
+  async runInTransaction(...statements: Statement[]): Promise<RunResult> {
+    const release = await this.txMutex.acquire()
+    return new Promise<RunResult>((resolve, reject) => {
+      this.db
+        .withTransactionAsync(async () => {
+          const results = await Promise.all(
+            statements.map(this._run.bind(this))
+          )
+          const runResult = results.reduce((resA, resB) => ({
+            rowsAffected: resA.rowsAffected + resB.rowsAffected,
+          }))
+          resolve(runResult)
+        })
+        .catch(reject)
+    }).finally(release)
+  }
+
+  async transaction<T>(
+    f: (_tx: Tx, setResult: (res: T) => void) => void
+  ): Promise<T> {
+    const release = await this.txMutex.acquire()
+    return new Promise<T>((resolve, reject) => {
+      this.db
+        .withTransactionAsync(
+          () =>
+            new Promise((txnResolve, txnReject) => {
+              const adaptedTxn = new WrappedTx(this, txnReject)
+              f(adaptedTxn, (res) => {
+                txnResolve()
+                resolve(res)
+              })
+            })
+        )
+        .catch(reject)
+    }).finally(release)
+  }
+}
+
+class WrappedTx implements Tx {
+  constructor(
+    private tx: DatabaseAdapter,
+    private signalFailure: (err: any) => void
+  ) {}
+  run(
+    statement: Statement,
+    successCallback?: (tx: Tx, res: RunResult) => void,
+    errorCallback?: (error: any) => void
+  ): void {
+    this.tx
+      ._run(statement)
+      .then((res) => {
+        successCallback?.(this, res)
+      })
+      .catch((err) => {
+        errorCallback?.(err)
+        this.signalFailure(err)
+      })
+  }
+
+  query(
+    statement: Statement,
+    successCallback: (tx: Tx, res: Row[]) => void,
+    errorCallback?: (error: any) => void
+  ): void {
+    this.tx
+      ._query(statement)
+      .then((res) => {
+        successCallback?.(this, res)
+      })
+      .catch((err) => {
+        errorCallback?.(err)
+        this.signalFailure(err)
+      })
   }
 }
